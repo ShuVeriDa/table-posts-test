3b32a83bb5bdce4fcecdc352985d7d05125f0150
ShuVeriDa <bashtarov@outlook.com>
2023-07-27 18:02:51 +0300

    fixed bugs

--- src/utils/useDebounce.ts
+++ src/utils/useDebounce.ts
@@ -1,10 +1,15 @@
 import {useCallback} from "react";
 import debounce from "lodash.debounce";
+import {useDispatch} from "react-redux";
+import {AppDispatchType} from "../redux/store.ts";
+import {setCurrentPage} from "../redux/table/tableSlice.ts";
 
 export const useDebounce = (callback: (value: string) => void, delay: number) => {
+  const dispatch = useDispatch<AppDispatchType>();
   return useCallback(
     debounce((str: string) => {
       callback(str);
+      dispatch(setCurrentPage(1))
     }, delay),
     []
   );
